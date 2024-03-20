c339b800d47c6f8d5eb8b4fdbb87bdcf8d52a398
tanupats <tanupat_s@kkumail.com>
2024-03-21 01:43:32 +0700

    add new service

--- routes/food.js
+++ routes/food.js
@@ -2,29 +2,62 @@ var express = require("express");
 var router = express.Router();
 const connection = require("../connection.js");
 
-router.get("*", async (req, res, next) => {
-    let path = req.path;
-    console.log(path)
+// router.get("*", async (req, res, next) => {
+//     let path = req.path;
+//     console.log(path)
 
-   
-    connection.query("SELECT * FROM `services` WHERE pathName=? AND methods='GET'", [path], (err, result) => {
-        if (err) {
-            console.log(err)
-        }
 
-       
-        let {queryData,params } = {...result[0]};
-        
-        console.log(params)
-        //get parameter from path 
+//     connection.query("SELECT * FROM `services` WHERE pathName=? AND methods='GET'", [path], (err, result) => {
+//         if (err) {
+//             console.log(err)
+//         }
 
-        connection.query(queryData, (err, result) => {
-            if (err) {
-                console.log(err)
-            }
-            res.send(result);
-        });
-    });
+
+//         let { queryData, params } = { ...result[0] };
+
+//         console.log(params)
+//         //get parameter from path 
+
+//         connection.query(queryData, (err, result) => {
+//             if (err) {
+//                 console.log(err)
+//             }
+//             res.send(result);
+//         });
+
+//     });
+// });
+let Rotepaths = "/:pathName"
+router.post("/setService",(req,res)=>{
+
+})
+
+router.get(Rotepaths, async (req, res, next) => {
+ 
+    let param = req.query;
+    console.log(param);
+
+
+    
+    
+    // connection.query("SELECT * FROM `services` WHERE pathName=? AND methods='GET'", [path], (err, result) => {
+    //     if (err) {
+    //         console.log(err)
+    //     }
+
+
+    //     let { queryData, params } = { ...result[0] };
+
+    //     //console.log(params)
+    //     //get parameter from path 
+
+    //     connection.query(queryData, (err, result) => {
+    //         if (err) {
+    //             console.log(err)
+    //         }
+    //         res.send(result);
+    //     });
+    // });
 });
 
 
