8a0a82e0c8d46f4b8c7be0fc223bd6aac2c3b23a
papanuwit <papanuwit@kkumail.com>
2024-03-26 00:58:19 +0700

    add method put

--- routes/food.js
+++ routes/food.js
@@ -7,70 +7,114 @@ router.get("*", async (req, res, next) => {
     console.log(path)
     //GET path form url to 
     connection.query("SELECT * FROM services WHERE pathName=? AND methods='GET'", [path], (err, result) => {
-        if(result?.length>0){      
-        let { queryData, params } = { ...result[0] };
-        //get parameter from path 
-        if (params !== "") {
-            console.log("have parameter is")
-           
-            let para = "";
-          
+        if (result?.length > 0) {
+            let { queryData, params } = { ...result[0] };
+            //get parameter from path 
+            if (params !== "") {
+                console.log("have parameter is")
 
-            for (const key in req.query) {
-                para=req.query[key];
-              }
-              
-            connection.query(queryData, [para], (err, result) => {
-                if (err) {
-                    console.log(err)
-                }
-                res.send(result);
-            });
-        }else{
-            connection.query(queryData,(err, result) => {
-                if (err) {
-                    console.log(err)
+                let para = "";
+
+
+                for (const key in req.query) {
+                    para = req.query[key];
                 }
 
-                res.send(result);
-            });
+                connection.query(queryData, [para], (err, result) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    res.send(result);
+                });
+            } else {
+                connection.query(queryData, (err, result) => {
+                    if (err) {
+                        console.log(err)
+                    }
+
+                    res.send(result);
+                });
+            }
+        } else {
+            res.send({ message: "services not found " + path });
         }
-    }else {       
-        res.send({message:"services not found "+path});
-    }
     });
 });
 
 
 //POST
-router.post("*", async (req,res) => {
+router.post("*", async (req, res) => {
 
     let path = req.path;
     let body = req.body;
     //GET path form url to 
     connection.query("SELECT * FROM services WHERE pathName=? AND methods='POST'", [path], (err, result) => {
-        if(result.length>0){      
-        let { queryData,fields,dataModel} = { ...result[0] };
+        if (result.length > 0) {
+            let { queryData, fields, dataModel } = { ...result[0] };
 
 
-        let bodyPost =[];    
-        //get body from post
-        for (let key in body) {
-            console.log(key + ': ' + body[key]);
-            bodyPost.push(body[key])
-          }
-          console.log(bodyPost)
-            connection.query(queryData,bodyPost,(err) => {
+            let bodyPost = [];
+            //get body from post
+            for (let key in body) {
+                console.log(key + ': ' + body[key]);
+                bodyPost.push(body[key])
+            }
+            console.log(bodyPost)
+            connection.query(queryData, bodyPost, (err) => {
                 if (err) {
                     console.log(err)
                 }
-                res.send({message:"Insert Data success In Table "+dataModel});
+                res.send({ message: "Insert Data success In Table " + dataModel });
             });
-       
-    }else {       
-        res.send({message:"cant NOT POST path"+path});
-    }
+
+        } else {
+            res.send({ message: "cant NOT POST path" + path });
+        }
     });
+});
+
+
+//PUT METHOD 
+router.put("*", async (req, res) => {
+    //full paths
+    let path = req.path;
+    let body = req.body;
+
+
+    let appPath = '/'+path.split("/")[1]
+     //console.log('app path/', path.split("/")[1])
+    //console.log('parameter',path.split("/").slice(2))
+    let newPata = path.split("/").slice(2)
+    console.log(newPata)
+
+    //GET path form url to 
+    connection.query("SELECT * FROM services WHERE pathName=? AND methods='PUT'", [appPath], (err, result) => {
+        if (result.length > 0) {
+            let { queryData, dataModel } = { ...result[0] };
+            let bodyPost = [];
+
+            //get body from put 
+            for (let key in body) {
+                console.log(key + ': ' + body[key]);
+                bodyPost.push(body[key])
+            }
+
+            bodyPost.push(newPata[0])
+
+            console.log(bodyPost)
+
+            connection.query(queryData, bodyPost, (err) => {
+                if (err) {
+                    console.log(err)
+                }
+                res.send({ message: "Update Data success In Table " + dataModel });
+            });
+
+        } else {
+            res.send({ message: "Cant NOT PUT path" + path });
+        }
+    });
+   
 });
 
 
