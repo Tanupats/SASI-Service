46f66742574b66de8711a7bee2a7491e8df7bf41
Tanupat Sittiwong <69186299+Tanupats@users.noreply.github.com>
2024-04-27 18:11:50 +0700

    Update connection.js
    ewedewde

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "sasirest",
+    host: "aws.connect.psdb.cloud",
+    user: "qhlqn7jo1fq72j6vektp",
+    password: "pscale_pw_ltFNGMaNkhxwVqvdknU6n9UBcTpniYzu3PjZkwCOoR",
+    database: "sasi_delivery",
 });
         connection.connect((err) => {
     if (err) {
