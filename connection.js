bcc4a1c7f6b48f152dbc415209020146dd15aec4
Tanupat Sittiwong <tsittiwong@ck-think.com>
2024-04-28 01:57:00 +0700

    add env file

--- connection.js
+++ connection.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
-const connection = mysql.createConnection('mysql://qhlqn7jo1fq72j6vektp:pscale_pw_ltFNGMaNkhxwVqvdknU6n9UBcTpniYzu3PjZkwCOoRj@aws.connect.psdb.cloud/sasi_delivery?ssl={"rejectUnauthorized":true}');
+require('dotenv').config()
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 connection.connect((err) => {
     if (err) {
         console.log("Connect Erro", err);
