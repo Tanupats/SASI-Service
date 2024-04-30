978ab2f9f07914f93e334473e8e2f3f8d4671b34
Tanupat Sittiwong <tsittiwong@ck-think.com>
2024-05-01 01:02:17 +0700

    edit sericesd

--- connection.js
+++ connection.js
@@ -1,8 +1,10 @@
 const mysql = require("mysql2");
 require('dotenv').config()
 const connection = mysql.createConnection(process.env.API_KEY);
+//const  connection = mysql.createConnection({host:"localhost",user:'root',password:'',database:'sasirest'});
 connection.connect((err) => {
     if (err) {
+      
         console.log("Connect Erro", err);
         return;
 
